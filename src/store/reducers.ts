da0655aad3f4d46f3cbd69defb5d1b22a134edd2
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-08 23:03:01 -0300

    refactor: remove files unnecessary

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,4 +1,3 @@
 export { exampleSimple } from './exampleSimple/slice'
 export { exampleAsync } from './exampleAsyncSlice/slice'
-export { exampleAsyncThunk } from './exampleAsyncThunk/slice'
 export { exampleWithPayload } from './exampleWithPayload/slice'
