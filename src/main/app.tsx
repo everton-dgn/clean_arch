c321e262f036e2364f86676cfac1652e249b39b8
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-09 00:58:11 -0300

    feat: add main layer

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from 'store/configStore'
 import GlobalStyles from 'styles/GlobalStyles'
@@ -8,7 +8,10 @@ import GlobalStyles from 'styles/GlobalStyles'
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
-      <BrowserRouter>{/* <Routes /> */}</BrowserRouter>
+      <h1>Hello World!</h1>
+      {/* <BrowserRouter> */}
+      {/*  <Routes /> */}
+      {/* </BrowserRouter> */}
       <GlobalStyles />
     </PersistGate>
   </Provider>
