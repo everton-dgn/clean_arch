b026c92ad343d3137aa43d4f2295f8590bbe5354
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-14 02:46:44 -0300

    feat: add colors in theme

--- src/presentation/theme/colors.ts
+++ src/presentation/theme/colors.ts
@@ -5,5 +5,7 @@ export const colors = {
   white: '#fff',
   black: '#222',
   background: '#f2f2f2',
-  error: '#ff3a3a'
+  error: '#ff3a3a',
+  disabledBackground: '#ccc',
+  disabledColor: '#666'
 }
