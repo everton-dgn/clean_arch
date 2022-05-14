4304f291501cbee864237e25d4a3ecaf8a4de50c
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-14 02:52:54 -0300

    feat: add state of form status

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,7 +1,22 @@
 import * as C from 'presentation/components'
+import { useEffect, useState } from 'react'
 import * as S from './styles'
 
+export type FormStatusType = {
+  isLoading: boolean
+  errorMessage: string
+}
+
 const Login = () => {
+  const [formStatus, setFormStatus] = useState<FormStatusType>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
+  useEffect(() => {
+    formStatus.isLoading && setFormStatus({ ...formStatus })
+  }, [formStatus])
+
   return (
     <S.Login>
       <C.LoginHeader />
@@ -17,11 +32,13 @@ const Login = () => {
           placeholder="Digite a sua senha"
         />
 
-        <S.Button type="submit">Entrar</S.Button>
+        <S.Button type="submit" disabled={true}>
+          Entrar
+        </S.Button>
 
         <S.WrapperLink>Criar conta</S.WrapperLink>
 
-        <C.FormStatus />
+        <C.FormStatus formStatus={formStatus} />
       </S.Form>
 
       <C.Footer />
