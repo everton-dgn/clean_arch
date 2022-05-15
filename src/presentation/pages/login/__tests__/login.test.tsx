dfd691397af58fcca1b1e0775120e63ccaa21e76
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-15 03:12:45 -0300

    test: ensure Login starts with inputs with required state

--- src/presentation/pages/login/__tests__/login.test.tsx
+++ src/presentation/pages/login/__tests__/login.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import T from 'presentation/theme'
 import { renderWithProviders } from 'presentation/utils'
 import Login from '..'
 
@@ -11,9 +12,15 @@ describe('Login', () => {
     const buttonSubmit = screen.getByRole('button', {
       name: /entrar/i
     })
+    const titleEmail = screen.queryByTitle(/campo de e-mail obrigatório/i)
+    const titlePassword = screen.queryByTitle(/campo de senha obrigatório/i)
 
     expect(spinner).not.toBeInTheDocument()
     expect(errorMessage).not.toBeInTheDocument()
     expect(buttonSubmit).toBeDisabled()
+    expect(titleEmail).toBeInTheDocument()
+    expect(titlePassword).toBeInTheDocument()
+    expect(titleEmail).toHaveStyle({ backgroundColor: T.colors.error })
+    expect(titlePassword).toHaveStyle({ backgroundColor: T.colors.error })
   })
 })
