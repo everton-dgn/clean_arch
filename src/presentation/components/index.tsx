d5cb4d4244cec381923ad54ef515ff102f11e356
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-28 16:18:02 -0300

    feat: add component ErrorBoundary

--- src/presentation/components/index.tsx
+++ src/presentation/components/index.tsx
@@ -3,3 +3,6 @@ export { default as FormStatus } from './formStatus'
 export { default as Input } from './input'
 export { default as Load } from './load'
 export { default as LoginHeader } from './loginHeader'
+export { default as ErrorBoundary } from './ErrorBoundary'
+
+export * from './templates'
